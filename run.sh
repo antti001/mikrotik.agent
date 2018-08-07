b49f930a8cfebd8f0e600ce7de986327fd11e9da
Home <antti.arrak@hotmail.com>
2018-08-07 06:45:29 +0300

    Script logging

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo "$(date) execute run.php" >> /tmp/mikrotik.agent.txt
+
 php $DIR/run.php
 sleep 30
 php $DIR/run.php
