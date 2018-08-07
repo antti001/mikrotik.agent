498711ef28c3bc3bfa0dae780f2ce53dfbed8d56
Home <antti.arrak@hotmail.com>
2018-08-07 07:06:07 +0300

    Script upgrade

--- git_upgrade.sh
+++ git_upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 LOGFILE="/tmp/mikrotik.agent.log"
-TIMESTAMP=`date "%d.%m %H:%M:%S"`
+TIMESTAMP=`date "+%d.%m %H:%M:%S"`
 echo "$TIMESTAMP execute git_upgrade.sh" >> $LOGFILE
 #git reset --hard origin/master
 #git clean -fxd
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 LOGFILE="/tmp/mikrotik.agent.log"
-TIMESTAMP=`date "%d.%m %H:%M:%S"`
+TIMESTAMP=`date "+%d.%m %H:%M:%S"`
 echo "$TIMESTAMP execute run.sh $1" >> $LOGFILE
 
 php $DIR/run.php $1
