c9dee2a8c5488da218ce6497cc78ddf5466b4833
Home <antti.arrak@hotmail.com>
2018-08-07 06:53:20 +0300

    Script upgrade

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 LOGFILE="/tmp/mikrotik.agent.log"
 TIMESTAMP=`date "%d.%m %H:%M:%S"`
-echo "$TIMESTAMP execute run.php" >> $LOGFILE
+echo "$TIMESTAMP execute run.sh $1" >> $LOGFILE
 
-php $DIR/run.php
+php $DIR/run.php $1
 sleep 30
 php $DIR/run.php
